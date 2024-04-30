817cd05a97fc624e5ad7ec1953cf83c9041ded05
tomgurner <tomgurner@gmail.com>
2024-04-29 22:48:33 -0700

    W02 Abstract

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public string _startYear;
+    public string _endYear;
+
+    public void ShowJobDetails()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}.");
+    }
+}
+
